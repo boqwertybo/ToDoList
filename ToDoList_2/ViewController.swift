025ab0e31ba3e422c5f7e4fe3eef4c8899b19fde
Kovalenko Bogdan <d29048@yandex.ru>
2020-09-05 21:42:38 +0300

    Initial Commit

---
+++ ToDoList_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList_2
+//
+//  Created by Bogdan on 05.09.2020.
+//  Copyright © 2020 Bogdan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
